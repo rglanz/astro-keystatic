d69dd96192f9d22d155f65f17a69be1a7f7e12da
Ryan <glanzrm@gmail.com>
2024-02-22 16:40:51 -0600

    convert to cloud development

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,10 @@ import { config, fields, singleton } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "cloud",
+  },
+  cloud: {
+    project: "cedar-studios/astro-keystatic",
   },
 
   singletons: {
